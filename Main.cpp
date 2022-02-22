b0ac4bc05b5ad933399ac7ecf4e077023f3f9cde
RomanManachynskyi <roman.manachynskyi@gmail.com>
2022-02-22 10:52:53 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <iostream>
+
+float minElement (int array[])
+{
+
+}
+float maxElement (int array[])
+{
+    
+}
+
+float minElementIndex (int array[])
+{
+    
+}
+
+float maxElementIndex (int array[])
+{
+    
+}
+
+
+int main()
+{
+    ifstream File = "Result.txt";
+
+
+    return 0;
+}
